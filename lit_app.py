4388ac1a364c395559980bea79c87f658fc78c53
Aniket Maurya <theaniketmaurya@gmail.com>
2022-07-22 15:54:27 +0530

    update

--- lit_app.py
+++ lit_app.py
@@ -2,8 +2,6 @@ import lightning as L
 from lightning.app.frontend import StreamlitFrontend
 
 def render_streamlit(app_state):
-    import os
-    os.system("pip uninstall opencv-python && pip install opencv-python-headless")
     import app
 
 class LitApp(L.LightningFlow):
